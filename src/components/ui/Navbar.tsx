d3e24824fea231e11174bfcdaa8ab72916ec9598
Aditya Singh <singhaditya8382@gmail.com>
2025-08-20 21:12:23 +0530

    refactor: clean up Navbar component formatting and improve code readability

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className='h-[10vh] w-full bg-white shadow-md flex justify-between'>
-            <div className='w-[20%] flex items-center gap-4 justify-center'>
-                <Image
-                    src='/Images/logo.svg'
-                    alt='Logo'
-                    width={50}
-                    height={50}
-                />
-            <div className='flex items-center gap-4'>
-                <h1 className='text-black text-2xl font-bold'>Volunteer App</h1>
-            </div>
-            </div>
-        <div className='w-[20%] flex items-center gap-4 justify-center'>
-          <a href="#" className='text-black'>Home</a>
-          <a href="#" className='text-black'>About</a>
-          <a href="#" className='text-black'>Contact</a>
+    <div className='flex h-[10vh] w-full justify-between bg-white shadow-md'>
+      <div className='flex w-[20%] items-center justify-center gap-4'>
+        <Image
+          src='/Images/logo.svg'
+          alt='Logo'
+          width={50}
+          height={50}
+        />
+        <div className='flex items-center gap-4'>
+          <h1 className='text-2xl font-bold text-black'>Volunteer App</h1>
         </div>
+      </div>
+      <div className='flex w-[20%] items-center justify-center gap-4'>
+        <a href="#" className='text-black'>Home</a>
+        <a href="#" className='text-black'>About</a>
+        <a href="#" className='text-black'>Contact</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
